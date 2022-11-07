b69cdeb63aace3738a508f89b802aab08e819b5e
Federico Luzzi <fedeluzzi00@gmail.com>
2022-11-07 11:03:58 +0000

    chore(contracts): update project structure and implement proper setup fork script

---
+++ packages/contracts/.cct/setup-fork.js
@@ -0,0 +1,88 @@
+import { utils, ContractFactory } from "ethers";
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+
+const require = createRequire(fileURLToPath(import.meta.url));
+
+export const setupFork = async (
+    factory,
+    kpiTokensManager,
+    _oraclesManager,
+    _multicall,
+    predictedTemplateId,
+    signer
+) => {
+    // deploy template
+    const {
+        abi: templateAbi,
+        bytecode: templateBytecode,
+    } = require("../artifacts/ERC20KPIToken.sol/ERC20KPIToken.json");
+    const templateFactory = new ContractFactory(
+        templateAbi,
+        templateBytecode,
+        signer
+    );
+    const templateContract = await templateFactory.deploy();
+    await templateContract.deployed();
+
+    // deploy creation proxy
+    const {
+        abi: creationProxyAbi,
+        bytecode: creationProxyBytecode,
+    } = require("../artifacts/CreationProxy.sol/CreationProxy.json");
+    const creationProxyFactory = new ContractFactory(
+        creationProxyAbi,
+        creationProxyBytecode,
+        signer
+    );
+    const creationProxy = await creationProxyFactory.deploy(
+        factory.address,
+        kpiTokensManager.address,
+        predictedTemplateId
+    );
+    await creationProxy.deployed();
+
+    // deploy test erc20 tokens
+    const {
+        abi: erc20Abi,
+        bytecode: erc20Bytecode,
+    } = require("../artifacts/ERC20PresetMinterPauser.sol/ERC20PresetMinterPauser.json");
+    const erc20Factory = new ContractFactory(erc20Abi, erc20Bytecode, signer);
+    const testToken1Contract = await erc20Factory.deploy(
+        "Test token 1",
+        "TST1"
+    );
+    await testToken1Contract.deployed();
+    const testToken2Contract = await erc20Factory.deploy(
+        "Test token 2",
+        "TST2"
+    );
+    await testToken2Contract.deployed();
+
+    // mint some test erc20 tokens to signer
+    await testToken1Contract.mint(signer.address, utils.parseUnits("100", 18));
+    await testToken2Contract.mint(signer.address, utils.parseUnits("100", 18));
+
+    return {
+        templateContract,
+        customContracts: [
+            {
+                name: "Creation proxy",
+                address: creationProxy.address,
+            },
+            {
+                name: "ERC20 1",
+                address: testToken1Contract.address,
+            },
+            {
+                name: "ERC20 2",
+                address: testToken2Contract.address,
+            },
+        ],
+        frontendGlobals: {
+            CREATION_PROXY_ADDRESS: creationProxy.address,
+            ERC20_1_ADDRESS: testToken1Contract.address,
+            ERC20_2_ADDRESS: testToken2Contract.address,
+        },
+    };
+};
---
+++ packages/contracts/src/Dependencies.sol
@@ -0,0 +1,4 @@
+// this contract imports dependencies needed
+// to setup the local fork
+
+import {ERC20PresetMinterPauser} from "oz/token/ERC20/presets/ERC20PresetMinterPauser.sol";
