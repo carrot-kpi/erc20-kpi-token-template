3536d3005ae4ea7e7b6a7b24cdc9bebbe8dd0fc7
Federico Luzzi <fedeluzzi00@gmail.com>
2022-11-16 15:27:31 +0000

    chore(frontend): rename frontend globals (typerscript related)

--- packages/frontend/src/carrot-template-env.d.ts
+++ packages/frontend/src/carrot-template-env.d.ts
@@ -3,7 +3,7 @@ import 'carrot-scripts'
 import { Address } from 'wagmi'
 
 declare global {
-  const ERC20_1_ADDRESS: Address
-  const ERC20_2_ADDRESS: Address
-  const CREATION_PROXY_ADDRESS: Address
+  const CCT_ERC20_1_ADDRESS: Address
+  const CCT_ERC20_2_ADDRESS: Address
+  const CCT_CREATION_PROXY_ADDRESS: Address
 }
