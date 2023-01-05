f9817e533b3f26e5dbc290aa40e85bb187748636
Paolo Guerra <pg.paologuerra@gmail.com>
2023-01-05 15:23:28 +0100

    refactor(frontend): remove spread

--- packages/frontend/src/creation-form/components/oracles-picker/index.tsx
+++ packages/frontend/src/creation-form/components/oracles-picker/index.tsx
@@ -57,7 +57,7 @@ export const OraclesPicker = ({
             );
 
             if (!pickedOracle) {
-                return nextOracleTemplates;
+                return previousState;
             }
 
             pickedOracle.isPicked = !pickedOracle.isPicked;
