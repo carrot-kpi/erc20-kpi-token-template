c7104f07230e1937bb2a0c18b04b264f6a451e60
Paolo Guerra <pg.paologuerra@gmail.com>
2023-01-27 17:48:12 +0100

    refactor(frontend): make campaign card modular

--- packages/frontend/src/page/index.tsx
+++ packages/frontend/src/page/index.tsx
@@ -7,7 +7,12 @@ import { ReactComponent as ShareIcon } from "../assets/share.svg";
 import { ReactComponent as ReportIcon } from "../assets/report.svg";
 
 import "../global.css";
-import { OverviewCard } from "./components/overview-card";
+import { CampaignCardExpanded } from "../ui/campaign-card-expanded";
+import { shortenAddress } from "../utils/address";
+import { InfoSection } from "../ui/campaign-card-expanded/info-section";
+import { Header } from "../ui/campaign-card-expanded/info-section/header";
+import { Content } from "../ui/campaign-card-expanded/info-section/content";
+import { formatDate } from "../utils/dates";
 
 interface PageProps {
     i18n: i18n;
@@ -48,7 +53,84 @@ export const Component = ({ t, kpiToken }: PageProps): ReactElement => {
                         {t("report")}
                     </Button>
                 </div>
-                <OverviewCard t={t} kpiToken={kpiToken} />
+                <CampaignCardExpanded
+                    description={kpiToken.specification.description}
+                    tags={kpiToken.specification.tags}
+                >
+                    <InfoSection>
+                        <Header>
+                            <Typography variant="sm" uppercase>
+                                {t("overview.rewards.label")}
+                            </Typography>
+                        </Header>
+                        <Content>
+                            <div className="flex justify-between">
+                                <Typography variant="sm" uppercase>
+                                    {t("overview.contract.label")}
+                                </Typography>
+                                <Typography variant="sm">
+                                    {shortenAddress(kpiToken.address)}
+                                </Typography>
+                            </div>
+                            <div className="flex justify-between">
+                                <Typography variant="sm" uppercase>
+                                    {t("overview.creator.label")}
+                                </Typography>
+                                <Typography variant="sm">{"..."}</Typography>
+                            </div>
+                        </Content>
+                    </InfoSection>
+                    <InfoSection>
+                        <Header>
+                            <Typography variant="sm" uppercase>
+                                {t("overview.time.label")}
+                            </Typography>
+                        </Header>
+                        <Content>
+                            <div className="flex justify-between">
+                                <Typography variant="sm" uppercase>
+                                    {t("overview.token.label")}
+                                </Typography>
+                                <Typography variant="sm" uppercase>
+                                    {"..."}
+                                </Typography>
+                            </div>
+                            <div className="flex justify-between">
+                                <Typography variant="sm" uppercase>
+                                    {t("overview.supply.label")}
+                                </Typography>
+                                <Typography variant="sm" uppercase>
+                                    {"..."}
+                                </Typography>
+                            </div>
+                        </Content>
+                    </InfoSection>
+                    <InfoSection>
+                        <Header>
+                            <Typography variant="sm" uppercase>
+                                {t("overview.participants.label")}
+                            </Typography>
+                        </Header>
+                        <Content>
+                            <div className="flex justify-between">
+                                <Typography variant="sm" uppercase>
+                                    {t("overview.resolution.label")}
+                                </Typography>
+                                <Typography variant="sm">
+                                    {formatDate(new Date(kpiToken.expiration))}
+                                </Typography>
+                            </div>
+                            <div className="flex justify-between">
+                                <Typography variant="sm" uppercase>
+                                    {t("overview.reward.label")}
+                                </Typography>
+                                <Typography variant="sm" uppercase>
+                                    {"..."}
+                                </Typography>
+                            </div>
+                        </Content>
+                    </InfoSection>
+                </CampaignCardExpanded>
             </div>
             <div className="bg-white bg-grid-white dark:bg-black m-5 px-2 py-3 sm:px-9 sm:py-5 md:px-36 md:py-24">
                 <Typography variant="h2">{t("account.title")}</Typography>
