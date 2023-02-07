b40035118a90ed78b7a379e3d743aa5f23b8af59
Federico Luzzi <fedeluzzi00@gmail.com>
2023-02-07 10:18:30 +0000

    feat(frontend): scoped template css

--- packages/frontend/tailwind.config.cjs
+++ packages/frontend/tailwind.config.cjs
@@ -1,10 +1,15 @@
 const { join } = require("path");
+const { long: longCommitHash } = require("git-rev-sync");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    important: `#${longCommitHash(__dirname)}`,
     content: [
         join(__dirname, "./playground/**/*.{js,jsx,ts,tsx}"),
         join(__dirname, "./src/**/*.{js,jsx,ts,tsx}"),
     ],
     presets: [require("@carrot-kpi/ui/tailwind-preset")],
+    corePlugins: {
+        preflight: false,
+    },
 };
