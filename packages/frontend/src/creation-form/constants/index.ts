a3411601f9283555eef4ba27a4e2f701114a4846
Federico Luzzi <fedeluzzi00@gmail.com>
2023-02-24 13:01:42 +0000

    chore(frontend): update sepolia creation proxy address and move it to constants

--- packages/frontend/src/creation-form/constants/index.ts
+++ packages/frontend/src/creation-form/constants/index.ts
@@ -1,4 +1,12 @@
+import { ChainId } from "@carrot-kpi/sdk";
+import { Address } from "wagmi";
+
 const COINGECKO_LIST = "https://tokens.coingecko.com/uniswap/all.json";
 const CARROT_LIST = "https://carrot-kpi.dev/token-list.json";
 
 export const TOKEN_LIST_URLS: string[] = [CARROT_LIST, COINGECKO_LIST];
+
+export const CREATION_PROXY_ADDRESS: Record<ChainId, Address> = {
+    [ChainId.GOERLI]: "0x66ad026c2dAF2A7CbF265f7E1804712bb250F549",
+    [ChainId.SEPOLIA]: "0x60Dbf4e0Ab6194F1c3a6dcbA48522FF654c2d95f",
+};
--- packages/frontend/src/creation-form/index.tsx
+++ packages/frontend/src/creation-form/index.tsx
@@ -21,11 +21,7 @@ import { OraclesConfiguration } from "./components/oracles-configuration";
 import { OutcomesConfiguration } from "./components/outcomes-configuration";
 import { Deploy } from "./components/deploy";
 import { SquareStepsList } from "../ui/square-steps-list";
-
-const CREATION_PROXY_ADDRESS: Record<ChainId, Address> = {
-    [ChainId.GOERLI]: "0x66ad026c2dAF2A7CbF265f7E1804712bb250F549",
-    [ChainId.SEPOLIA]: "0x4300d4C410f87c7c1824Cbc2eF67431030106604",
-};
+import { CREATION_PROXY_ADDRESS } from "./constants";
 
 interface CreationFormProps {
     i18n: i18n;
