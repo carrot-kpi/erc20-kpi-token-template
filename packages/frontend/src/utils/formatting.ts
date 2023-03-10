16d30c3255072a59b404bbd7ac9708f679f59e43
Federico Luzzi <fedeluzzi00@gmail.com>
2023-03-10 10:01:45 +0000

    fix(frontend): fix format units bug

--- packages/frontend/src/utils/formatting.ts
+++ packages/frontend/src/utils/formatting.ts
@@ -1,8 +1,8 @@
 import { Amount, Token } from "@carrot-kpi/sdk";
-import { commify } from "ethers/lib/utils.js";
+import { commify, formatUnits } from "ethers/lib/utils.js";
 
 export const formatTokenAmount = (amount: Amount<Token>, withSymbol = true) => {
-    let baseAmount = commify(amount.toString());
+    let baseAmount = commify(formatUnits(amount.raw, amount.currency.decimals));
     if (withSymbol) baseAmount = baseAmount + ` ${amount.currency.symbol}`;
     return baseAmount;
 };
