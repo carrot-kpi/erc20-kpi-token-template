d08ea190d49fa4e78f6d748ebc0060e6174575e6
Paolo Guerra <pg.paologuerra@gmail.com>
2023-03-22 21:29:49 +0100

    feat(frontend): add length validations to generic data step inputs

--- packages/frontend/src/creation-form/constants/index.ts
+++ packages/frontend/src/creation-form/constants/index.ts
@@ -12,6 +12,11 @@ export const TOKEN_LIST_URLS: string[] = [CARROT_LIST, COINGECKO_LIST];
 export const PROTOCOL_FEE_BPS = 30;
 
 export const MAX_KPI_TOKEN_DESCRIPTION_CHARS = 256;
+export const MAX_KPI_TOKEN_TITLE_CHARS = 64;
+export const MAX_KPI_TOKEN_TAG_CHARS = 16;
+export const MAX_KPI_TOKEN_ERC20_NAME_CHARS = 24;
+export const MAX_KPI_TOKEN_ERC20_SYMBOL_CHARS = 16;
+export const MAX_KPI_TOKEN_TAGS_COUNT = 10;
 
 export const CREATION_PROXY_ADDRESS: Record<ChainId, Address> = {
     [ChainId.GNOSIS]: "0xc9CC9a4d4F2c57F0d47c169A3d96D47FfFe5E0b6",
