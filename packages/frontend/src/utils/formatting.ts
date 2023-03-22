d5815a9626eefcbac63716e254f3460681ac6f8d
Federico Luzzi <fedeluzzi00@gmail.com>
2023-03-22 09:34:34 +0000

    feat(frontend): update format token amount to limit the number of non-zero decimals shown

--- packages/frontend/src/utils/formatting.ts
+++ packages/frontend/src/utils/formatting.ts
@@ -1,8 +1,30 @@
 import { Amount, Token } from "@carrot-kpi/sdk";
 import { commify, formatUnits } from "ethers/lib/utils.js";
 
-export const formatTokenAmount = (amount: Amount<Token>, withSymbol = true) => {
-    let baseAmount = commify(formatUnits(amount.raw, amount.currency.decimals));
-    if (withSymbol) baseAmount = baseAmount + ` ${amount.currency.symbol}`;
-    return baseAmount;
+export const formatTokenAmount = (
+    amount: Amount<Token>,
+    withSymbol = true,
+    nonZeroDecimalsAmount = 4
+) => {
+    let rawBaseAmount = formatDecimals(
+        commify(formatUnits(amount.raw, amount.currency.decimals)),
+        nonZeroDecimalsAmount
+    );
+    if (withSymbol)
+        rawBaseAmount = rawBaseAmount + ` ${amount.currency.symbol}`;
+    return rawBaseAmount;
+};
+
+const formatDecimals = (rawBaseAmount: string, decimalsAmount = 4) => {
+    const decimalIndex = rawBaseAmount.indexOf(".");
+    if (decimalIndex === -1) return rawBaseAmount;
+    let i = decimalIndex + 1;
+    while (i < rawBaseAmount.length) {
+        if (rawBaseAmount[i] !== "0") {
+            i += decimalsAmount;
+            break;
+        }
+        i++;
+    }
+    return rawBaseAmount.substring(0, i);
 };
